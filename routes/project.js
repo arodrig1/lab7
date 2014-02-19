20c39e18f36e29a8c733344cb75974a0d43d6dc3
Alejandro Rodriguez <arodrig1@stanford.edu>
2014-02-19 22:50:08 +0000

    databases lab

--- routes/project.js
+++ routes/project.js
@@ -2,13 +2,12 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
+  console.log(projectID);
+  models.Project.findById(projectID).exec(afterQuery);
 
-  // query for the specific project and
-  // call the following callback
-
-  function afterQuery(err, projects) {
+  function afterQuery(err, project) {
     if(err) console.log(err);
-    res.json(projects[0]);
+    res.json(project);
   }
 }
 
@@ -16,13 +15,36 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
-  // make a new Project and save it to the DB
-  // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project({
+    "title": form_data["project_title"],
+    "date": form_data["date"],
+    "summary": "<p>" + form_data["summary"] + "</p>",
+    "image": form_data["image_url"]
+  });
+
+  newProject.save(afterSaving);
+
+  function afterSaving(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    }
+    res.send(200);
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
-  // find the project and remove it
+  models.Project.findById(projectID).remove().exec(afterRemoving);
+
+  function afterRemoving(err) {
+    if(err) {
+      console.log(err);
+      res.send(500);
+    }
+    res.send(200);
+  }
+
   // YOU MUST send an OK response w/ res.send();
 }
